7e876617a7f2cdd9cd974a8fe6b0294b39dfff57
Jen Rogers <jen@sci.utah.edu>
2020-11-17 22:38:24 -0700

    save

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.util.List;
+import java.util.ArrayList;
+
 public class BinarySearchTree {
 
     class Node{
@@ -15,6 +20,69 @@ public class BinarySearchTree {
     Node root;
 
     BinarySearchTree(){ root = null; }
+
+    List<List<Integer>> levelOrderBottom(Node root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null) return res;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            List<Integer> list = new ArrayList();
+            int size = queue.size();
+            for(int i = 0; i<size; i++) {
+                Node node = queue.poll();
+                list.add(node.key);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(0, list);
+        }
+        return res;
+    }
+
+    void bfsPrint(Node node){
+        int h = getHeight(node);
+        for(int i = 1; i <= h; i++){
+            printLevel(node, i);
+        }
+    }
+
+    int getHeight(Node node){
+        if(node == null){
+            return 0;
+        }else{
+            int leftH = getHeight(node.left);
+            int rigH = getHeight(node.right);
+
+            if(leftH > rigH) return (leftH + 1);
+            else return(rigH + 1);
+        }
+    }
+
+    void printLevel(Node node, int in){
+        if(node ==  null){
+            return;
+        }
+        if(in == 1){
+            System.out.println(node.key + " ");
+        }else if(in > 1){
+            printLevel(node.left, in-1);
+            printLevel(node.right, in-1);
+        }
+    }
+
+    Node mirror(Node node){
+        if(node == null){
+            return node;
+        }
+        Node left = mirror(node.left);
+        Node right = mirror(node.right);
+
+        node.left = right;
+        node.right = left;
+
+        return node;
+    }
 
     void insert(int key){
         root = insertRec(root, key);
@@ -45,7 +113,6 @@ public class BinarySearchTree {
             inOrderRec(node.right);
         }
     }
-
     public static void main(String[] args){
 
         BinarySearchTree tree = new BinarySearchTree();
@@ -54,8 +121,34 @@ public class BinarySearchTree {
         tree.insert(40);
         tree.insert(70);
         tree.insert(100);
+        tree.insert(20);
+        tree.insert(44);
 
         tree.inOrder();
+
+        int h = tree.getHeight(tree.root);
+        System.out.println("h:  " + h);
+
+        // tree.mirror(tree.root);
+
+        // System.out.println("reverse reverse: ");
+        // tree.inOrder();
+
+        // System.out.println("testing BFS: ");
+
+        // tree.bfsPrint(tree.root);
+
+        List<List<Integer>> test = tree.levelOrderBottom(tree.root);
+
+        System.out.println(test + " " + test.get(0));
+
+        for(int i = 0; i < test.size(); i++){
+            String s = "";
+            for(int j = 0; j < test.get(i).size(); j++){
+                s = s+ ", " +test.get(i).get(j);
+            }
+            System.out.println("level" + s);
+        }
       
 
     }
